6c42b646dd67a7ab8cdcb61800f3a0c7e0729858
difo23 <diffozanzan@gmail.com>
2020-07-18 23:51:50 -0400

    Build docs para Github Page

---
+++ docs/precache-manifest.c500c5d8f2651accf0b0f12ea42c9f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c5d3d8f02c5daa1e165361e9da9a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c919f8433731405ce2f",
+    "url": "/static/css/main.b545ecd9.chunk.css"
+  },
+  {
+    "revision": "230fa27fc83f868a8457",
+    "url": "/static/js/2.096a8fee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.096a8fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c919f8433731405ce2f",
+    "url": "/static/js/main.31028d7b.chunk.js"
+  },
+  {
+    "revision": "c2a4463da067d7ee0a4c",
+    "url": "/static/js/runtime-main.f3c16400.js"
+  }
+]);
